861d479f34836e97ac2025d573243a1f6cdf6955
ArivanLG <Arivan_lessa@hotmail.com>
2023-05-22 23:21:39 -0300

    Primeiro Projeto

---
+++ Calculadora/src/main/java/operacoes/Calculadora.java
@@ -0,0 +1,25 @@
+package operacoes;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		
+		// divisao /
+		// multiplicação *
+		// som +
+		// subtração -
+		
+		OperacoesMatematicas op = new  OperacoesMatematicas();
+		
+		op.divisao(10,2);
+		op.multiplicacao(10,30);
+		op.soma(5,30);
+		op.subtracao(5,40);
+		
+		
+		
+		
+		
+	}
+
+}
